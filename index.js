ab39e74a3dd3ba78821e6cb92b9136eec17a0386
Kviponder <119442089+Kviponder@users.noreply.github.com>
2023-04-30 19:48:25 -0400

    Added js to connect the shapes.js file to index.js

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+const shapes = require("./lib/shapes");
 const inquirer = require("inquirer");
+const jest = require("jest");
 const fs = require("fs");
 
 
-console.log("hello");
+const { Circle, Square, Triangle} = require("./lib/shapes");
+const circle = new Circle();
+const square = new Square();
+const triangle = new Triangle();
 
+console.log(triangle.render());
+console.log(circle.render());
+console.log(square.render());
+
+
+// myShape.setColor("red");
+// console.log(myShape)
+
+//Ideas for questionaire
 const questions = [
   {
     type: "input",
@@ -18,44 +32,66 @@ const questions = [
       }
     },
   },
+  //The choice will be tied to the circle class, square class, triangle class, etc, and then input it using Jquery into the string literl creating the svg ie
+
+  //
   {
     type: "list",
     name: "shape",
     message: "Please select a shape for your project logo.",
-    choices: ["square", "circle", "triangle"], //maybe add "diamond"
+    choices: Object.keys(shapes),
   },
-    {  // work on this to enable a user to either choose a color or enter a hex code
-        type: "input list",
-        name: "color",
-        message: "What color would you like your logo to be?",
-        list: ["red", "blue", "green", "yellow", "purple", "orange", "black", "white", 
-        "I have a specific color in mind,(click to enter a hex code)"]
-    }
+  {
+    // work on this to enable a user to either choose a color or enter a hex code
+    type: "input list",
+    name: "color",
+    message: "What color would you like your logo to be?",
+    list: [
+      "red",
+      "blue",
+      "green",
+      "yellow",
+      "purple",
+      "orange",
+      "black",
+      "white",
+      "I have a specific color in mind,(click to enter a hex code)",
+    ],
+  },
 ];
+inquirer.prompt(questions).then((answers) => {
+  console.log(answers.title);
+  console.log(answers.shape);
+  console.log(answers.color);
+  const logo = new shapes(answers.title, answers.shape, answers.color);
+  console.log(logo);
+  fs.writeFile("logo.svg", logo);
+});
+
+
+// return writeToFile(("logo.svg,", data));
 
 //below was taken from readme-generator, can easily be modified to fit our needs
 
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) => {
-      // if there's an error, log it, otherwise log a success message
-  
-      err
-        ? console.error(err)
-        : console.log("You've successfully created a logo as an SVG file");
-    });
-  }
-  
+  fs.writeFile(fileName, data, function (err) {
+    // if there's an error, log it, otherwise log a success message
+    err
+      ? console.error(err)
+      : console.log("You've successfully created a logo as an SVG file");
+  });
+}
 
-  function init() {
-    inquirer
-      .prompt(questions)
-      .then((data) => {
-        const markdownContent = generateMarkdown(data);
-        writeToFile( `${selectedLicense.badge}.svg)`, markdownContent);
-      })
-      .catch((err) => console.log(err));
-  }
-  
-  // Function call to initialize app
-  init();
-  
+// function to initialize program
+function init() {
+  inquirer
+    .prompt(questions)
+    .then((data) => {
+      const markdownContent = generateMarkdown(data);
+      writeToFile(`${selectedLicense.badge}.svg)`, markdownContent);
+    })
+    .catch((err) => console.log(err));
+}
+
+// Function call to initialize app
+init();
