7ac5b54bad7b85ef4a51356f263e961a3f5d144b
Kviponder <119442089+Kviponder@users.noreply.github.com>
2023-05-01 12:23:12 -0400

    added final functionality, need to adjust circle still

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
-const shapes = require("./lib/shapes");
 const inquirer = require("inquirer");
-const jest = require("jest");
 const fs = require("fs");
+const {Circle, Square, Triangle} = require("./lib/shapes");
 
-
-const { Circle, Square, Triangle} = require("./lib/shapes");
-const circle = new Circle();
-const square = new Square();
-const triangle = new Triangle();
-
-console.log(triangle.render());
-console.log(circle.render());
-console.log(square.render());
-
-
-// myShape.setColor("red");
-// console.log(myShape)
-
-//Ideas for questionaire
+//Prompts questions to be asked to the user and exported to index.js
 const questions = [
   {
     type: "input",
     name: "title",
-    message: "What is the name of your project?",
+    message: "What is the name of your logo, enter up to three characters?",
     validate: (value) => {
       if (value.trim().length > 0 && value.trim().length <= 3) {
         // if the user doesn't enter a title, the prompt will repeat
@@ -32,6 +17,12 @@ const questions = [
       }
     },
   },
+  {
+    type: "input",
+    name: "textcolor",
+    message: "What color would you like your text to be?",
+  },
+
   //The choice will be tied to the circle class, square class, triangle class, etc, and then input it using Jquery into the string literl creating the svg ie
 
   //
@@ -39,37 +30,53 @@ const questions = [
     type: "list",
     name: "shape",
     message: "Please select a shape for your project logo.",
-    choices: Object.keys(shapes),
+    choices: ["Circle", "Triangle", "Square"],
+    default: "circle",
+    name: "shape",
   },
+
   {
     // work on this to enable a user to either choose a color or enter a hex code
-    type: "input list",
+    type: "input",
     name: "color",
     message: "What color would you like your logo to be?",
-    list: [
-      "red",
-      "blue",
-      "green",
-      "yellow",
-      "purple",
-      "orange",
-      "black",
-      "white",
-      "I have a specific color in mind,(click to enter a hex code)",
-    ],
   },
 ];
-inquirer.prompt(questions).then((answers) => {
-  console.log(answers.title);
-  console.log(answers.shape);
-  console.log(answers.color);
-  const logo = new shapes(answers.title, answers.shape, answers.color);
-  console.log(logo);
-  fs.writeFile("logo.svg", logo);
-});
+let shape;
+let shapeText = "";
 
 
-// return writeToFile(("logo.svg,", data));
+// function to initialize program
+function init() {
+  return inquirer.prompt(questions).then((answers) => {
+    switch (answers.shape) {
+      case "Circle":
+        shape = new Circle();
+        shape.setColor(answers.color);
+        shapeText = shape.render();
+        break;
+      case "Triangle":
+        shape = new Triangle();
+        shape.setColor(answers.color);
+        shapeText = shape.render();
+        break;
+      case "Square":
+        shape = new Square();
+        shape.setColor(answers.color);
+        shapeText = shape.render();
+        break;
+    }
+    let svgCon = `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+${shapeText}
+
+<text x="150" y="170" font-size="50" text-anchor="middle" fill="${answers.textcolor}">${answers.title}</text>
+
+</svg>`;
+
+    return writeToFile("logo.svg", svgCon);
+  });
+} 
 
 //below was taken from readme-generator, can easily be modified to fit our needs
 
@@ -80,17 +87,6 @@ function writeToFile(fileName, data) {
       ? console.error(err)
       : console.log("You've successfully created a logo as an SVG file");
   });
-}
-
-// function to initialize program
-function init() {
-  inquirer
-    .prompt(questions)
-    .then((data) => {
-      const markdownContent = generateMarkdown(data);
-      writeToFile(`${selectedLicense.badge}.svg)`, markdownContent);
-    })
-    .catch((err) => console.log(err));
 }
 
 // Function call to initialize app
